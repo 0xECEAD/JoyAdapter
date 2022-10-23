0d7b3d1c5f59486fd7aebe824d5549269f535db9
Enceladus <enc@seynen.net>
2022-10-23 15:15:28 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,110 @@
-# JoyAdapter
-8-bit Atari Joystick to USB adapter
+# JoyAdapter (c) 2019 Enceladus 
+
+Another 8-bit Atari Joystick to USB adapter.
+
+![](Img/IMGP0105showcase.jpg)
+
+There are dozens of similar converters available, but I wanted one in a sleek form factor.
+It is about the size of the iconic Atari connector itself. 
+
+It is a standard USB-HID class (Joystick) device, generally no drivers required. It has 2 buttons and 2-axis (non configurable). No POT/Paddle inputs only digital (switch to ground) inputs. 
+
+The LED flashes on connect and movement/button action.
+
+
+## PCB:
+
+![pcb](Img/board.jpg)
+
+### Pinout J1 Atari:
+Connector  [Wikipedia](https://en.wikipedia.org/wiki/Atari_joystick_port)
+
+| Pin | Function |
+| ------ | ------ |
+| 1 | Up |
+| 2 | Down |
+| 3 | Left |
+| 4 | Right |
+| 5 | N.C. |
+| 6 | Button 1 |
+| 7 | +5V |
+| 8 | Ground |
+| 9 | Button 2 |
+
+**Note:** +5V on pin 7 (not fused or limitied or protected) can damage you USB host if shorted.
+5V is not required for most (passive) joysticks i know, so you could pull pin 7 the out of the connector to be safe.
+
+### Parts: 
+
+|Qty|Ref(s)|Value|Manufacturer|Note|Datasheet|
+| ------ | ------ | ------ | ------ | ------ | ------ |
+|1|C1|4u7F||Tantalum||
+|1|C2|1uF||||
+|1|C3|100nF||||
+|1|D1|L-424IDT|Kingbright|FlatTop|[pdf](https://nl.mouser.com/datasheet/2/216/L-424IDT(Ver.12A)-795377.pdf)|
+|1|H1|MountingHole||2.5mm||
+|1|J1|DB9 Male||Solder Straight, shell removed||
+|1|J3|USB PLUG Type "A"|Keystone Electronics|SMD|[pdf](https://www.mouser.com/datasheet/2/215/31-744008.pdf)|
+|2|J4, J5|Connector 1x2||pitch 1.27mm||
+|1|R1|68Ω||||
+|1|U1|XC6220B331MR|Torex Semiconductor||[pdf](https://www.torexsemi.com/file/xc6220/XC6220.pdf)|
+|1|U2|STM32F042G6Ux|STMicroelectronics|Package QFN28 4x4mm|[pdf](http://www.st.com/st-web-ui/static/active/en/resource/technical/document/datasheet/DM00105814.pdf)|
+
+Notes: 
+- The J1 shell is actually a **DE**-type (common mistake, even KiCad fails this)
+- Any low-drop voltage regulator for U1, (SOT23-5 package), either 3V or 3V3, e.q. XC6220B331MR or AP7383-30W5.
+- U2 is a STM32F042G6Ux controller, it features crystal-less USB, and 5V tolerant inputs.
+- The SWD programming connector was split into two, J4 and J5 due to size restrictions.
+
+
+### Construction:
+
+![pcb](Img/JoyAdapterD08.png)
+
+Manufacture the PCB with FR4 thickness 1.2mm. (Standard 1.6mm will not fit between the pins of J1).
+
+J1 is a standard DSUB 9pin Male connector. Remove the shell with cutters and pliers and some force.
+The remaining inner part can be mounted with the PCB between the pins. 
+
+Solder the LED so the flat top aligns nicely with the top side of the case.
+
+## Case:
+
+![pcb](Img/Case040.png)
+
+Print the Bottom and Top part of the case on your 3D printer (i used a Ultimaker S5).
+Use a M2.5 x 8mm Hex Socket Head Cap Screw to join bottom case and top case together.
+
+
+## Software:
+
+Project setup was done with [STM CubeMX](https://www.st.com/en/development-tools/stm32cubemx.html), further implementation with [VisualGDB](https://visualgdb.com/).
+The [Descriptor Tool](https://www.usb.org/document-library/hid-descriptor-tool) was used to design the HID Report.
+Its a fairly simple source code, you can adjust it to you favorite development environment.
+
+### Latency:
+
+Updates 100x per second, reason mostly to reduce contact bounce. 
+In theory you could send at 1kHz for LowSpeed USB or 8kHz for FullSpeed USB, but you may want to include some debounce logic.
+Current implementation thus updates every change in a 10ms interval, faster then most screen refresh rates of retro games. 
+
+
+### Compatibility
+Windows 98/XP/Vista/7/8/10/11, Mac OSX, Linux, RaspberryPI, MiSTer. 
+
+
+## Tested Devices
+The Arcade by S.T.C. Rotterdam
+
+
+## Design Software
+
+KiCad v6.0.7 + GerberView v2.6.1
+FreeCAD v0.19
+Ultimaker Cura v4.6.2
+
+Descriptor Tool, Version 2.4
+VisualGDB v5.6r8 + STM CubeMX v6.5
+
+
+[[Img/JoyAdapter.mkv]]
---
+++ Source/Src/main.c
@@ -0,0 +1,333 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usb_device.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "usbd_custom_hid_if.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim14;
+
+/* USER CODE BEGIN PV */
+volatile int flag = 0;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_TIM14_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim)
+{
+   flag = 1;
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+  
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM2_Init();
+  MX_TIM14_Init();
+  MX_USB_DEVICE_Init();
+  /* USER CODE BEGIN 2 */
+
+   int buttons = GPIOB->IDR & (JOY_LEFT_Pin | JOY_RIGHT_Pin | JOY_UP_Pin | JOY_DOWN_Pin | JOY_FIRE1_Pin | JOY_FIRE2_Pin);
+
+   uint8_t report_data[USBD_CUSTOMHID_OUTREPORT_BUF_SIZE];
+   //int delay = 75;
+   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
+   HAL_TIM_Base_Start_IT(&htim14);
+   int pwm = 999;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+   while (1)
+   {
+      if (flag == 0) continue;
+      flag = 0;      // 100x per sec
+
+      if (pwm > 80) pwm -= 80; else pwm = 0;
+      TIM2->CCR2 = pwm;
+
+      int bs2 = GPIOB->IDR & (JOY_LEFT_Pin | JOY_RIGHT_Pin | JOY_UP_Pin | JOY_DOWN_Pin | JOY_FIRE1_Pin | JOY_FIRE2_Pin);
+      if (bs2 != buttons)
+      {
+         buttons = bs2;
+         report_data[0] = ((buttons & JOY_FIRE1_Pin) == 0 ? 1 : 0) | ((buttons & JOY_FIRE2_Pin) == 0 ? 2 : 0);
+         report_data[1] = (buttons & JOY_LEFT_Pin) == 0 ? -127 : (buttons & JOY_RIGHT_Pin) == 0 ? 127 : 0;
+         report_data[2] = (buttons & JOY_UP_Pin) == 0 ? -127 : (buttons & JOY_DOWN_Pin) == 0 ? 127 : 0;
+         HID_SendReport(report_data);
+
+         if (pwm < 750) pwm += 250; else pwm = 1000;
+         TIM2->CCR2 = pwm;
+      }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+   }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+  RCC_CRSInitTypeDef RCC_CRSInitStruct = {0};
+
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
+
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Enable the SYSCFG APB clock 
+  */
+  __HAL_RCC_CRS_CLK_ENABLE();
+  /** Configures CRS 
+  */
+  RCC_CRSInitStruct.Prescaler = RCC_CRS_SYNC_DIV1;
+  RCC_CRSInitStruct.Source = RCC_CRS_SYNC_SOURCE_USB;
+  RCC_CRSInitStruct.Polarity = RCC_CRS_SYNC_POLARITY_RISING;
+  RCC_CRSInitStruct.ReloadValue = __HAL_RCC_CRS_RELOADVALUE_CALCULATE(48000000,1000);
+  RCC_CRSInitStruct.ErrorLimitValue = 34;
+  RCC_CRSInitStruct.HSI48CalibrationValue = 32;
+
+  HAL_RCCEx_CRSConfig(&RCC_CRSInitStruct);
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 479;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 999;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 500;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+  HAL_TIM_MspPostInit(&htim2);
+
+}
+
+/**
+  * @brief TIM14 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM14_Init(void)
+{
+
+  /* USER CODE BEGIN TIM14_Init 0 */
+
+  /* USER CODE END TIM14_Init 0 */
+
+  /* USER CODE BEGIN TIM14_Init 1 */
+
+  /* USER CODE END TIM14_Init 1 */
+  htim14.Instance = TIM14;
+  htim14.Init.Prescaler = 4799;
+  htim14.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim14.Init.Period = 99;
+  htim14.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim14.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim14) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM14_Init 2 */
+
+  /* USER CODE END TIM14_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pins : JOY_FIRE2_Pin JOY_UP_Pin JOY_FIRE1_Pin JOY_DOWN_Pin 
+                           JOY_LEFT_Pin JOY_RIGHT_Pin */
+  GPIO_InitStruct.Pin = JOY_FIRE2_Pin|JOY_UP_Pin|JOY_FIRE1_Pin|JOY_DOWN_Pin 
+                          |JOY_LEFT_Pin|JOY_RIGHT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+   /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(char *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+   /* User can add his own implementation to report the file name and line number,
+      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
